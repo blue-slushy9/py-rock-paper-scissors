2857b7ab28323c7a52c40bc94314a3e67ef5c34e
blue-slushy9 <ra.cruz971@gmail.com>
2023-06-22 01:07:40 +0000

    Made a few changes while waiting for my train! Still have to keep working on it, but it's cleaner now, also added some exception handling.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,36 +1,57 @@
 # Play rock-paper-scissors against the computer!
 
-# Hint: involves the random module
+# The random.choice() method allows you to pick an element at random, in this
+# case from a list;
+from random import choice
 
-import random
-
+# Throughout this program, I use many print() statements to enhance legibility
+# in the terminal, e.g. below;
+print()
 print("This is Rock-Paper-Scissors! You will be playing against the computer. Please enter your selection now...")
+print()
+
+# We use lower() to control for human error in the input, e.g. "ROCK";
 human = input().lower()
+print()
 
 objects = ["rock","paper","scissors"]
 
-computer = random.choice(objects).lower()
+computer = choice(objects).lower()
 
 print(f"You chose {human}, and the computer chooses {computer}.")
 print()
 
-if human == "rock" and computer == "paper":
-    print("Paper covers rock, therefore you lose!")
+if human in objects:
 
-elif human == "rock" and computer == "scissors":
-    print("Rock breaks scissors, therefore you win!")
+    if human == "rock" and computer == "paper":
+        print("Paper covers rock, therefore you lose!")
+        print()
 
-elif human == "paper" and computer == "scissors":
-    print("Scissors cut paper, therefore you lose!")
+    elif human == "rock" and computer == "scissors":
+        print("Rock breaks scissors, therefore you win!")
+        print()
 
-elif human == "paper" and computer == "rock":
-    print("Paper covers rock, therefore you win!")
+    elif human == "paper" and computer == "scissors":
+        print("Scissors cut paper, therefore you lose!")
+        print()
 
-elif human == "scissors" and computer == "rock":
-    print("Rock breaks scissors, therefore you lose!")
+    elif human == "paper" and computer == "rock":
+        print("Paper covers rock, therefore you win!")
+        print()
 
-elif human == "scissors" and computer == "paper":
-    print("Scissors cut paper, therefore you win!")
+    elif human == "scissors" and computer == "rock":
+        print("Rock breaks scissors, therefore you lose!")
+        print()
+
+    elif human == "scissors" and computer == "paper":
+        print("Scissors cut paper, therefore you win!")
+        print()
+
+    else:
+        print("Both you and the computer chose the same object, therefore it's a stalemate.")
+        print()
 
 else:
-    print("Both you and the computer chose the same object, therefore it's a stalemate.")
+    print("This is an invalid input, please run the program again from the\n"
+            "beginning.")
+    print()
